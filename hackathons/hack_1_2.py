c13d285c78571f5c619cbf95bfcec8ec6baf3f2d
LucyOwen <lucywowen@gmail.com>
2025-01-30 15:05:02 -0700

    added vscode debugger tutorial

---
+++ hackathons/hack_1_2.py
@@ -0,0 +1,61 @@
+
+# Imports 
+import numpy as np
+
+t_max = 50e-3   # second
+dt = 1e-3        # second
+tau = 20e-3      # second
+el = -60e-3      # milivolt
+vr = -70e-3      # milivolt
+vth = -50e-3     # milivolt
+r = 100e6        # ohm
+i_mean = 25e-11  # ampere
+
+
+# Set random number generator
+np.random.seed(2020)
+
+# Initialize step_end, t_range, n, v_n and i
+t_range = np.arange(0, t_max, dt)
+step_end = len(t_range)
+n = 5
+v_n = el * np.ones([n, step_end])
+i = i_mean * (1 + 0.1 * (t_max / dt)**(0.5) * (2 * np.random.random([n, step_end]) - 1))
+
+# Initialize binary numpy array for raster plot
+raster = np.zeros([n,step_end])
+
+# Initialize t_ref and last_spike
+t_ref = 0.01
+last_spike = -t_ref * np.ones([n])
+
+# Loop over time steps
+for step, t in enumerate(t_range):
+
+  # Skip first iteration
+  if step == 0:
+    continue
+
+  # Compute v_n
+  v_n[:, step] = v_n[:, step - 1] + (dt / tau) * (el - v_n[:, step - 1] + r*i[:, step])
+
+  # Initialize boolean numpy array `spiked` with v_n > v_thr
+  spiked = (v_n[:,step] >= vth)
+
+  if any(spiked):
+    print('pause') # Added this just so we can see what happens when one of the neurons spikes
+
+  # Set relevant values of v_n to v_reset using spiked
+  v_n[spiked,step] = vr
+
+  # Set relevant elements in raster to 1 using spiked
+  raster[spiked,step] = 1.
+
+  # Initialize boolean numpy array clamped using last_spike, t and t_ref
+  clamped = (last_spike + t_ref > t) #(last_spike is -.01 and t_ref is .01 so it's 0 until its overwritten 
+
+  # Reset clamped neurons to vr using clamped
+  v_n[clamped,step] = vr
+
+  # Update numpy array last_spike with time t for spiking neurons
+  last_spike[spiked] = t
